0e089e984ecff1eaaadabd9ea6f0602cb04c2ee9
Chamomile <dltldms5271@gmail.com>
2023-11-09 16:53:19 +0900

    06

--- main.c
+++ main.c
@@ -4,28 +4,19 @@
 
 int main(int argc, char *argv[])
 {
-    FILE* fp; //file pointer
-    char str[100]; //문자 길이 대충 
-    int i; 
-    
-    //1. open file
-    fp = fopen("C:/Users/dltld/Documents/sample.txt", "w"); //두가지 입력 필요. 이름이랑 mode
-    
-    //2. write file
-    for(i=0; i<3; i++)
-    {
-        //2-1. print "input a word"
-        printf("input a word : ");
-        //2-2. scanf a string
-        scanf("%s", str);
-        //2-3. fprintf()
-        fprintf(fp, "%s\n", str);
-    }
-    
-    //3. close file
-    fclose(fp);
-
-   //sample.txt가 있는지 확인. 
+   FILE* fp;
+   char c;
+   fp = fopen("C:/Users/dltld/Documents/sample.txt", "r");
+   if(fp == NULL)
+   {
+         printf("failed to open\n");
+         return 0;
+   }
+   
+   while ( (c=fgetc(fp)) != EOF )
+   {
+         putchar(c);
+   }
 
   system("PAUSE");	
   return 0;
