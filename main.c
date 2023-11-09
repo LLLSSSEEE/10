55829480b60ae9cf4cd2fbed6146cb97d9d60ab3
Chamomile <dltldms5271@gmail.com>
2023-11-09 16:13:50 +0900

    05

--- main.c
+++ main.c
@@ -4,10 +4,29 @@
 
 int main(int argc, char *argv[])
 {
-    char src[30] = "happy C programming";
+    FILE* fp; //file pointer
+    char str[100]; //문자 길이 대충 
+    int i; 
+    
+    //1. open file
+    fp = fopen("sample.txt", "W"); //두가지 입력 필요. 이름이랑 mode
+    
+    //2. write file
+    for(i=0; i<3; i++)
+    {
+        //2-1. print "input a word"
+        printf("input a word : ");
+        //2-2. scanf a string
+        scanf("%s", str);
+        //2-3. fprintf()
+        fprintf(fp, "%s\n", str);
+    }
+    
+    //3. close file
+    fclose(fp);
 
-    printf("문자열\"%s\"의 길이 : %i\n", src, strlen(src));
-   
+   //sample.txt가 있는지 확인. 
+
   system("PAUSE");	
   return 0;
 }
